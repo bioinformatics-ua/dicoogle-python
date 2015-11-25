693d573d947e18106b2f727271a9cf03fe8081fb
Tiago Marques Godinho <tmgodinho@ua.pt>
2015-11-25 10:26:46 +0000

    Added version 2.0
    Major Cleanup : Most of the old methods were deprecated
    They will be ported in due time

--- dicoogle.py
+++ dicoogle.py
@@ -1,80 +1,55 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-How works Dicoogle Webservices in REST?
-
-Search by Date Range – Access images in date 2005/03/29:
-URL: http://www.dicoogle.com/services/dws/dim?advq=StudyDate:[20050329 TO 20050329]
-
-Search by Date Range – Access images in date 2005/03/29 and Modality CT (Computer Tomography):
-URL: http://www.dicoogle.com/services/dws/dim?advq=Modality:CT AND StudyDate:[20050329 TO 20050329]
-
-Access parameters for each image (use uid):
-
-http://www.dicoogle.com/services/dws/dump?uid=1.3.12.2.1107.5.1.4.54023.30000005032914013107800000965
-
-Search Free Text – looking for CT keyword:
-
-http://www.dicoogle.com/services/dws/dim?q=CT
-
-Get DICOM File:
-
-http://www.dicoogle.com/services/dws/file?uid=1.3.12.2.1107.5.1.4.54023.30000005032914013107800000965
-
-"""
-
 __title__ = 'dicoogle'
-__version__ = '0.1-dev'
-__author__ = 'Luís A. Bastião Silva'
+__version__ = '2.0-dev'
+__author__ = 'Luís A. Bastião Silva, Tiago Marques Godinho'
 __license__ = 'GPL v3'
 __copyright__ = 'Copyright 2013, Luís Bastião, Universidade de Aveiro'
 __url__ = 'http://bastiao.github.io/dicoogle-python/'
 
-__maintainer__ = 'Luís A. Bastião Silva'
-__email__ = 'bastiao@ua.pt'
+__maintainer__ = 'Luís A. Bastião Silva, Tiago Marques Godinho'
+__email__ = 'bastiao@ua.pt, tmgodinho@ua.pt'
 
 __all__ = ()
 
-ENDPOINT = "http://localhost:6060/"
-
-import sys
-import time
-import json
-try:
-    from urllib.parse import quote
-except ImportError:
-    from urllib import quote  # NOQA
-import xmltodict, json
-
-import requests  # urllib2 sucks badly, we depend on requests
-
-def search_freetext(query):
-	result = requests.get(ENDPOINT + "dim?q="+ query)
-	o = xmltodict.parse(result.text)
-	return o
-
-def search_advanced(query):
-	result = requests.get(ENDPOINT + "dim?advq="+ query)
-	o = xmltodict.parse(result.text)
-	return result.text
-
-def dump(sop_instance_uid):
-	result = requests.get(ENDPOINT + "dim?advq="+ query)
-	o = xmltodict.parse(result.text)
-	return result.text	
-
-def download(sop_instance_uid, destination="temporary_file"):
-	# TODO 
-	pass
-
-def number_of_results(query):
-	result = requests.get(ENDPOINT + "dim?q="+ query)
-	return result.text
-
-#def wado(...):#
-#	pass
+import requests
 
 
-#print search_freetext("*:*")
+class Dicoogle:
+    def __init__(self, url='demo.dicoogle.com'):
+        self.ENDPOINT = "http://" + url
 
+    # Validated
+    def search_dim(self, query, provider=None, keyword=True):
+        #http://localhost:8080/searchDIM?query=Modality:CR&keyword=true
+        url = self.ENDPOINT + "/searchDIM"
+        data = {"query": query, "keyword": keyword}
+        if provider:
+            data["provider"] = provider
+
+        r = requests.get(url, params=data)
+        return r
+
+    #Validated
+    def export_csv(self, query, fields=[], providers=[], file=None):
+        url = self.ENDPOINT + "/export"
+        data = {"query": query, "fields": fields, "providers": providers}
+        r = requests.get(url, params=data, stream=True)
+        if file:
+            file.write(r.content)
+            file.flush()
+
+        return r
+
+    def search(self, query, provider, keyword=False):
+        url = self.ENDPOINT + "/search"
+        data = {"query": query, "provider": provider, "keyword": keyword}
+        r = requests.get(url, params=data);
+        return r
+
+    def dump(self, uid):
+        url = self.ENDPOINT + "/dump"
+        data = {"uid": uid}
+        r = requests.get(url, params=data)
+        return r
